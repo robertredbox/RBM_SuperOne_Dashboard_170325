efa72e842c30a0a96e8c70251a5b10ff42d71dbf
robertredbox <robert@redboxmobile.com>
2025-03-17 11:25:30 +0000

    Add empty Keywords component

---
+++ src/components/Keywords.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Keywords = () => {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6">
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">Keyword Analysis</h2>
+      <div className="p-4 bg-gray-50 rounded-lg border border-gray-200">
+        <p className="text-gray-500 text-center">Keyword analysis content will be added here.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Keywords;
